2cfcd795c4e4c5d1659901a613bb9038e23f3b35
chauhan-09 <nsc7574@gmail.com>
2026-04-27 05:39:47 +0530

    made changes

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const { productId } = await params;
+
+  return <h1>Product ID is: {productId}</h1>;
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Product(){
+    return (
+        <div>
+            <h1>this is the product page</h1>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+        </div>
+
+    );
+}
